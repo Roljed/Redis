ca367bfcb080ee27af53833c40f0afae1b4cc103
Yaad Nahshon <nahshon@Yaads-MacBook-Pro.local>
2024-08-30 23:11:22 +0300

    feat: add response to ping

--- app/main.py
+++ app/main.py
@@ -3,11 +3,22 @@ import socket
 
 ADDRESS = "localhost"
 REDIS_PORT = 6379
+PONG = "+PONG\r\n"
 
 
 def main():
     server_socket = socket.create_server((ADDRESS, REDIS_PORT), reuse_port=True)
-    server_socket.accept()
+    client_socket, address = server_socket.accept()
+    print(f"accepted connection from {address}")
+    
+    with client_socket:
+        # Read data
+        data = client_socket.recv(1024)
+
+        # Write data back
+        client_socket.sendall(PONG.encode())
+
+    server_socket.close()
 
 
 if __name__ == "__main__":
