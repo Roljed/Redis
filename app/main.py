7aa733d2171eaa01bb10d0455c0089fa99642532
Yaad Nahshon <nahshon@Yaads-MacBook-Pro.local>
2024-08-31 23:09:05 +0300

    feat: add concurrent clients using selectors

--- app/main.py
+++ app/main.py
@@ -1,24 +1,49 @@
 import socket
+import selectors
 
 
 ADDRESS = "localhost"
+BYTES_SIZE = 1024
 REDIS_PORT = 6379
 PONG = "+PONG\r\n"
 
 
+sel = selectors.DefaultSelector()
+
+
+def accept(server_socket):
+    client_socket, address = server_socket.accept()
+    print(f"Accepted connection from {address}")
+    client_socket.setblocking(False)
+    sel.register(client_socket, selectors.EVENT_READ, read)
+
+
+def read(client_socket):
+    request: bytes = client_socket.recv(BYTES_SIZE)
+    if request:
+        data: str = request.decode()
+        print(f"Received: {data}")
+        if "ping" in data.lower():
+            client_socket.sendall(PONG.encode())
+    else:
+        sel.unregister(client_socket)
+        client_socket.close()
+
+
 def main():
     server_socket = socket.create_server((ADDRESS, REDIS_PORT), reuse_port=True)
-    client_socket, address = server_socket.accept()
-    print(f"accepted connection from {address}")
-    
-    with client_socket:
-        # Read data
-        data = client_socket.recv(1024)
+    server_socket.listen()
+    server_socket.setblocking(False)
 
-        # Write data back
-        client_socket.sendall(PONG.encode())
+    sel.register(server_socket, selectors.EVENT_READ, accept)
 
-    server_socket.close()
+    print(f"Server is listening...")
+
+    while True:
+        events = sel.select()
+        for key, mask in events:
+            callback = key.data
+            callback(key.fileobj)
 
 
 if __name__ == "__main__":
